3ea052e8341032047276122db2fcf1b361e1c5a1
thedanielzhang <daniel.zhang@berkeley.edu>
2016-12-19 16:41:13 -0500

    Added Controller
    Also added spotify api .net library

---
+++ SpotiFind/Models/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SpotiFind.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Place { get; set; }
+        public string Playlist { get; set; }
+    }
+}
